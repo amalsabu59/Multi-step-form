71370b1216b699cc55cbfaa980a7e04375d5b6c4
amalsabu59 <amalsabu59@gmail.com>
2023-01-07 21:37:37 +0530

    selection of card and the slider logic added

--- src/MultiStepForm/Components/PlanDetails.js
+++ src/MultiStepForm/Components/PlanDetails.js
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PlanDetails.css";
 const archadeIcon = require("../../assets/images/icon-arcade.svg").default;
 const advancedIcon = require("../../assets/images/icon-advanced.svg").default;
 const proIcon = require("../../assets/images/icon-pro.svg").default;
 const data = [
-  { id: 1, icon: archadeIcon, plan: "Archade", price: "$9/mo" },
-  { id: 1, icon: advancedIcon, plan: "Advanced", price: "$12/mo" },
-  { id: 1, icon: proIcon, plan: "Pro", price: "$15/mo" },
+  {
+    id: 1,
+    icon: archadeIcon,
+    plan: "Archade",
+    price: "$9/mo",
+    selected: false,
+  },
+  {
+    id: 2,
+    icon: advancedIcon,
+    plan: "Advanced",
+    price: "$12/mo",
+    selected: false,
+  },
+  { id: 3, icon: proIcon, plan: "Pro", price: "$15/mo", selected: false },
+];
+const dataYearly = [
+  {
+    id: 1,
+    icon: archadeIcon,
+    plan: "Archade",
+    price: "$80/yr",
+    selected: false,
+  },
+  {
+    id: 2,
+    icon: advancedIcon,
+    plan: "Advanced",
+    price: "$120/yr",
+    selected: false,
+  },
+  { id: 3, icon: proIcon, plan: "Pro", price: "$150/yr", selected: false },
 ];
 const PlanDetails = () => {
+  const [planData, setPlanData] = useState(data);
+  const [isYearly, setIsYearly] = useState(false);
+  const handleSelect = (id) => {
+    const selctedItem = planData.map((item) => {
+      return item.id === id
+        ? {
+            ...item,
+            selected: !item.selected,
+          }
+        : { ...item };
+    });
+    return setPlanData(selctedItem);
+  };
+  const handleSlider = (e) => {
+    if (e.target.checked) {
+      setPlanData(dataYearly);
+      setIsYearly(true);
+    }
+    if (!e.target.checked) {
+      setPlanData(data);
+      setIsYearly(false);
+    }
+  };
   return (
     <div style={{ marginLeft: "100px" }}>
       <text>
@@ -18,9 +70,12 @@ const PlanDetails = () => {
         </p>
       </text>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        {data.map((item) => (
-          <div class="card">
-            <div class="container">
+        {planData.map((item) => (
+          <div
+            className={item.selected ? "card-selected" : "card"}
+            onClick={() => handleSelect(item.id)}
+          >
+            <div className="container">
               <div className="icon-container">
                 <img src={item.icon} alt="mySvgImage" />
               </div>
@@ -28,6 +83,7 @@ const PlanDetails = () => {
                 <b>{item.plan}</b>
               </h4>
               <p className="text-content">{item.price} </p>
+              {isYearly && <p>2 months free</p>}
             </div>
           </div>
         ))}
@@ -53,7 +109,7 @@ const PlanDetails = () => {
             marginRight: "10px",
           }}
         >
-          <input type="checkbox" />
+          <input type="checkbox" onChange={(e) => handleSlider(e)} />
           <span className="slider round"></span>
         </label>
         <p>Yearly</p>
