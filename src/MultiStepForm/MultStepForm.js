041ee00eee8b3954bc4a366cf1f3252e3d312f1e
amalsabu59 <amalsabu59@gmail.com>
2023-01-08 14:50:37 +0530

    multistep component updated

--- src/MultiStepForm/MultStepForm.js
+++ src/MultiStepForm/MultStepForm.js
@@ -3,12 +3,17 @@ import Button from "./Components/Button";
 import Form from "./Components/Form";
 import PlanDetails from "./Components/PlanDetails";
 import SideBar from "./Components/SideBar";
-
+import AddOns from "./Components/AddOns";
+import Summary from "./Components/Summary";
+import Thankyou from "./Components/ThankYou";
 const MultStepForm = () => {
   const [step, setStep] = useState(1);
   const components = [
     { component: Form, id: 1 },
     { component: PlanDetails, id: 2 },
+    { component: AddOns, id: 3 },
+    { component: Summary, id: 4 },
+    { component: Thankyou, id: 5 },
   ];
 
   return (
@@ -20,7 +25,13 @@ const MultStepForm = () => {
           return item.id === step && <item.component />;
         })}
 
-        <Button setStep={setStep} step={step} stepLength={components.length} />
+        {step <= 4 && (
+          <Button
+            setStep={setStep}
+            step={step}
+            stepLength={components.length}
+          />
+        )}
       </div>
     </div>
   );
